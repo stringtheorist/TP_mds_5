325e8515f812a5e443bc4a09224730474cdea2cf
martin-remiet <martin.remiet@etudiant.univ-rennes.fr>
2025-10-13 11:06:47 +0200

    Modif de ModePropre

--- ModePropre.m
+++ ModePropre.m
@@ -1,1 +1,12 @@
 function Y=ModePropre(kn,s,Nw,Aff)
+for in=1:Nw
+    % Y_ij, avec i=>n et j=>s
+    Y(in,:)=sin(kn(in)*s);
+end
+%=> visualisation de quelques modes propres
+figure(1);
+plot(s,Y([1:3 nmax],:),'LineWidth',2)
+xlabel('s [m]')
+legend('n=1','n=2','n=3','n=nmax')
+set(gca,'FontSize',24)
+end
