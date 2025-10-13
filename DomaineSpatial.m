9804db4a0fa71e264f05bcc974d2b56542cad1df
EhouarnDonniou <ehouarn.donniou@gmail.com>
2025-10-13 09:35:49 +0200

    crtlC crtlV domaines (modal,spatial,temporel)

--- DomaineSpatial.m
+++ DomaineSpatial.m
@@ -1,1 +1,5 @@
 function [ds,s,Ns]=DomaineSpatial(Lamb,L)
+  % Domaine spatial
+ds=min(Lamb)/20;% Pas en espace [m]
+s=(0:ds:L);     % Echantillonage spatial [m]
+Ns=length(s);   % Nombre de points d'echantillonages en espace
--- DomaineTemporel.m
+++ DomaineTemporel.m
@@ -1,1 +1,6 @@
 function [dt,t,Nt,tmax]=DomaineTemporel(Per,L)
+  % Domaine temporel
+dt=min(Per)/20; % Pas en temps [s]
+tmax=max(Per)*2;% Temps maximum de la simulation [s]
+t=0:dt:tmax;    % Echantillonage temporel [s]
+Nt=length(t);   % Nombre de points d'echantillonages en temps
