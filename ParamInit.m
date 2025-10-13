05bd43c0f4ccbb7b709718357ffb31942d714ea0
EhouarnDonniou <ehouarn.donniou@gmail.com>
2025-10-13 09:33:03 +0200

    crtlC crtlV les param sans comms

--- ParamInit.m
+++ ParamInit.m
@@ -1,1 +1,15 @@
 function [L,R,E,ro,Note,H,el,Nw,Aff]=ParamInit
+
+L=1;            % Longueur [m]
+R=0.001;        % Rayon [m]
+
+E=210e9;        % Module de Young [Pa]
+ro=7800;        % Masse volumique [kg/m^3]
+
+Note=440;       % Frequence fondamentale [Hz]
+H=L/5;          % Hauteur [m]
+el=L/4;         % poistion [m]
+
+nmax=10;        % Nombre maximal de mode considere
+n=(1:nmax)';    % Indices modaux
+Nw=nmax;
