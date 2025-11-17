fe909381ffcfc348eb059718f4225a8723570a93
martin-remiet <martin.remiet@etudiant.univ-rennes.fr>
2025-11-17 11:04:48 +0100

    J ai modifie les fonctions AmplitudeModale.m et Param.m

--- AmplitudeModale.m
+++ AmplitudeModale.m
@@ -2,8 +2,13 @@ function [an,bn]=AmplitudeModale(L,el,kn,wn,n,H,Aff)
 an=zeros(size(n));
 bn=L*V*(cos(kn.*el)+K/N0*sin(kn.*el)./kn)./normY2;
 %-> visualisation des amplitudes modales an
+if(Aff>0)
 figure(6);
 stem(Freq,abs(bn),'LineWidth',2)
 xlabel('fn [Hz]')
 ylabel('|bn| [m]')
 set(gca,'FontSize',24)
+elseif(Aff==0)
+disp('Le paramètre Aff=0 donc on n affiche pas les amplitudes modales');
+else
+    disp('Problème avec la variable Aff'); 
