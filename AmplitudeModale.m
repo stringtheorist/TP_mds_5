e499478e10dda4f7551df8197a4762cd7b59e6d1
martin-remiet <martin.remiet@etudiant.univ-rennes.fr>
2025-11-17 08:38:35 +0100

    <Commit message>

--- AmplitudeModale.m
+++ AmplitudeModale.m
@@ -1,1 +1,9 @@
 function [an,bn]=AmplitudeModale(L,el,kn,wn,n,H,Aff)
+an=zeros(size(n));
+bn=L*V*(cos(kn.*el)+K/N0*sin(kn.*el)./kn)./normY2;
+%-> visualisation des amplitudes modales an
+figure(6);
+stem(Freq,abs(bn),'LineWidth',2)
+xlabel('fn [Hz]')
+ylabel('|bn| [m]')
+set(gca,'FontSize',24)
